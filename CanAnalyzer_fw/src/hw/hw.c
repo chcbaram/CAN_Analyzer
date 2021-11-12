5ddee30e82407a772c3c4e58b2c9dd8c16b4e0b3
Hancheol Cho <chcbaram@gmail.com>
2021-11-13 00:01:06 +0900

    클럭 정보 출력 추가.

--- CanAnalyzer_fw/src/hw/hw.c
+++ CanAnalyzer_fw/src/hw/hw.c
@@ -29,7 +29,7 @@ bool hwInit(void)
   logPrintf("[ Firmware Begin... ]\r\n");
   logPrintf("Booting..Name \t\t: %s\r\n", _DEF_BOARD_NAME);
   logPrintf("Booting..Ver  \t\t: %s\r\n", _DEF_FIRMWATRE_VERSION);
-
+  logPrintf("Sys Clk       \t\t: %d Mhz\r\n", HAL_RCC_GetSysClockFreq()/1000000);
 
   logBoot(false);
 
